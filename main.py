4311cf906eca13e607e5905fd502d76961121a03
Smit Chawda <smitchawda@gmail.com>
2023-01-20 21:29:32 -0500

    DB connected and Collection located

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from pymongo import MongoClient
 
 client = MongoClient(mongo_cloud_connection_str.CONNECTION_STRING)
 db = client.test
+print('\n\nLOG: Connection Tested...\n\n')
+
+db = client[mongo_cloud_connection_str.DB_NAME]
+print('\n\nLOG: Connected to database...\n\n')
+
+collection = db[mongo_cloud_connection_str.COLLECTION_NAME]
+print('\n\nLOG: Collection located...\n\n')
