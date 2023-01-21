b6c96285bccdbb2cbaf3705bc7ebfcc475f2fa00
Smit Chawda <smitchawda@gmail.com>
2023-01-20 23:39:59 -0500

    Continuos document fetching implemented

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import predictor
 import mongo_cloud_connection_str
+
+import time
 from pymongo import MongoClient
+
+# Connecting to MongoDB
 
 client = MongoClient(mongo_cloud_connection_str.CONNECTION_STRING)
 db = client.test
@@ -11,3 +15,14 @@ print('\n\nLOG: Connected to database...\n\n')
 
 collection = db[mongo_cloud_connection_str.COLLECTION_NAME]
 print('\n\nLOG: Collection located...\n\n')
+
+one_record = None
+
+while one_record == None:
+    print('LOG: Blank document')
+    one_record = collection.find_one()
+    time.sleep(5)
+else:
+    print('LOG: Document Found')
+    # TODO: Document parsing logic goes here
+    
