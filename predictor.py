b01058d83207c60437ae38c13ad5293c9d517cb5
Smit Chawda <smitchawda@gmail.com>
2023-01-20 19:46:01 -0500

    Data cleansed - null values dropped

--- predictor.py
+++ predictor.py
@@ -10,20 +10,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 
 df = pd.read_csv('../input/credit-risk-analysis-for-extending-bank-loans/bankloans.csv')
-# Check the first 5 values of the dataset to ensure proper loading
-df.head()
-
-# Check for nulls in each column
-df.isnull().sum()
-
-# Dataset Description
-df.value_counts()
 
 # We dont want any null values in our project hence we drop them
 df = df.dropna()
-
-# Check again for nulls in each column
-df.isnull().sum()
 
 # Finally check for the number of defaults in the datasets
 df['default'].value_counts()
